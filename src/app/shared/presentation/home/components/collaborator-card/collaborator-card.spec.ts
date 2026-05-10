567870babae5cdede36bef38be485a112d00f798
Mr-Code-start <vitalybaca92@gmail.com>
2026-05-10 10:14:16 -0500

    feat(collaborator-card): add collaborator card component with styles and tests

---
+++ src/app/shared/presentation/home/components/collaborator-card/collaborator-card.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CollaboratorCard } from './collaborator-card';
+
+describe('CollaboratorCard', () => {
+  let component: CollaboratorCard;
+  let fixture: ComponentFixture<CollaboratorCard>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CollaboratorCard],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CollaboratorCard);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
