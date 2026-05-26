217a7bba0248c6a43fdcc30dbc5ef99538f19b67
Pieri <149537593+pierinaaa29@users.noreply.github.com>
2026-05-26 18:39:49 -0500

    chore(scripts): add npm run server script

--- server/start.sh
+++ server/start.sh
@@ -1,1 +1,4 @@
+#!/usr/bin/env bash
+# Permite correr el script desde cualquier directorio: cambia al directorio del script
+cd "$(dirname "$0")"
 json-server --watch db.json --routes routes.json --port 3000
